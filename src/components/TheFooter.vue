536d8f5587c3923aa55a6d77c029768a90565a84
RobledoJohn <robledoJohn@hotmail.com>
2024-09-20 15:57:46 -0500

    colocando redes sociales en el footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="footer-container">
+    <Router-link to="/subir" class="btn">
+      <li>Subir Fotos</li>
+    </Router-link>
     <div class="foot-one">
-      <p class="">Informacion Footer</p>
-      <Router-link to="/subir" class="btn">
-        <li>Subir Fotos</li>
-      </Router-link>
+      <p class="sub-tittle">Siguenos en:</p>
+      <div class="icons-foot">
+        <a href="https://www.tiktok.com/@jessikarobledo0" target="_blank"><i class="bi bi-tiktok icono-footer"></i></a>
+        <a href="https://www.instagram.com/jessy_spa_service/" target="_blank"><i class="bi bi-instagram icono-footer"></i></a>
+      </div>
     </div>
     <dic class="foot-two">
       <p>Desarrollado por &copy;SoycoDev</p>
@@ -18,12 +22,31 @@
   width: 100%;
 }
 
+.sub-tittle {
+  font-size: 20px;
+  margin: 5px;
+  color: #52021a;
+}
+
 .foot-one {
   background-color: #DD5981;
   color: #52021a ;
   height: 150px;
   display: flex;
   justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.icons-foot{
+  display: flex;
+  justify-content: center;
+}
+
+.icono-footer{
+  font-size: 30px;
+  margin: 10px;
+  color: white;
 }
 
 .foot-two{
