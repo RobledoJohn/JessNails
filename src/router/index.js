41b5e6ed5d4fc32473ae6a78da38da590fad5e86
RobledoJohn <robledoJohn@hotmail.com>
2024-10-12 18:27:49 -0500

    eliminando vista promociones

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ServicesView from '../views/ServicesView.vue'
 import ProductsView from '../views/ProductsView.vue'
-import PromosView from '../views/PromosView.vue'
 import GalleryView from '../views/GalleryView.vue'
 import Upload from '../views/UploadView.vue'
 
@@ -23,11 +22,6 @@ const router = createRouter({
       path: '/productos',
       name: 'products',
       component: ProductsView
-    },
-    {
-      path: '/promociones',
-      name: 'promos',
-      component: PromosView
     },
     {
       path: '/galeria',
