e30ca9eff8343ed9c04d72584097e72a7105efd9
RobledoJohn <robledoJohn@hotmail.com>
2024-10-12 18:13:35 -0500

    agregando descripcion a vista productos

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,10 +2,25 @@
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <p class="tittle">Productos</p>   
+
+    <p class="home-txt">En nuestra tienda encontrarás una amplia variedad de productos de alta calidad para el cuidado de tus manos y uñas. Nuestros productos son seleccionados cuidadosamente para garantizar que obtengas los mejores resultados. ¡Descubre nuestra colección y lleva a casa los productos que necesitas para mantener tus manos y uñas saludables y hermosas!</p>  
   </div>
 </template>
 
 <style scope>
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .home-txt{
+    font-size: 20px;
+    text-align: center;
+    margin: 0 10px;
+    width: 80%;
+  }
 </style>
